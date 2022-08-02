dfd376b01654ae472913263177288308c929e5c7
seripark13 <qkrtpfl9228@gmail.com>
2022-08-02 23:12:53 +0900

    ✨ feat: getTodos 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store/reducers";
 import { authActions } from "../store/slice";
 
 export default function Home() {
+  interface Todo {
+    content: string;
+    createdAt: string;
+    id: string;
+    title: string;
+    updatedAt: string;
+  }
   const dispatch = useDispatch();
+  const [todoList, setTodoList] = useState<Todo[]>([]);
   const auth = useSelector((state: RootState) => state.auth.auth);
+
+  useEffect(() => {
+    try {
+      const token = localStorage.getItem("token");
+      axios
+        .get("http://localhost:8080/todos", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((res) => setTodoList(res.data.data));
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   function logoutHandler() {
     dispatch(authActions.logout());
     localStorage.removeItem("token");
@@ -15,12 +40,37 @@ export default function Home() {
 
   return (
     <>
-      <Link to="/login">
-        <button>로그인</button>
-      </Link>
-      <Link to="/join">
-        <button>회원가입</button>
-      </Link>
+      {auth ? (
+        <div>
+          <button onClick={logoutHandler}>로그아웃</button>
+          <div>TODO</div>
+          <button onClick={openModalHandler}>생성</button>
+
+          {todoList &&
+            todoList.map((item) => (
+              <div key={item.id}>
+                <div>{item.title}</div>
+              </div>
+            ))}
+          {todoList.map((item) => (
+            <div key={item.id}>
+              <div>{item.title}</div>
+              <div>{item.content}</div>
+              <div>{item.createdAt}</div>
+              <div>{item.updatedAt}</div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <>
+          <Link to="/login">
+            <button>로그인</button>
+          </Link>
+          <Link to="/join">
+            <button>회원가입</button>
+          </Link>
+        </>
+      )}
     </>
   );
 }
