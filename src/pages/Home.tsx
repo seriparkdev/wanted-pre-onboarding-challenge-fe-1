c81bea0110d4cfa0900b2410bd5b3bf357db8e5d
seripark13 <qkrtpfl9228@gmail.com>
2022-08-03 20:00:50 +0900

    ✨ feat: updateTodo 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
@@ -17,9 +18,11 @@ export default function Home() {
   }
   const dispatch = useDispatch();
   const [todoList, setTodoList] = useState<Todo[]>([]);
-
+  const [isEdit, setIsEdit] = useState<boolean>(false);
   const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
   const [todoId, setTodoId] = useState<string>("");
+  const [newTitle, setNewTitle] = useState<string>("");
+  const [newContent, setNewContent] = useState<string>("");
   const auth = useSelector((state: RootState) => state.auth.auth);
 
   useEffect(() => {
@@ -46,6 +49,15 @@ export default function Home() {
     setIsOpenModal(!isOpenModal);
   }
 
+  function updateHandler(id: string) {
+    setIsEdit(!isEdit);
+    getTodoById(id);
+  }
+
+  function editModeHandler() {
+    setIsEdit(!isEdit);
+  }
+
   function getTodoById(id: string) {
     const token = localStorage.getItem("token");
     axios
@@ -57,6 +69,23 @@ export default function Home() {
       .then((res) => {
         setTodoId(res.data.data.id);
       });
+  }
+
+  function updateTodo(id: string) {
+    setIsEdit(!isEdit);
+    const token = localStorage.getItem("token");
+    axios.put(
+      `http://localhost:8080/todos/${id}`,
+      {
+        title: newTitle,
+        content: newContent,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
   }
   return (
     <>
@@ -81,6 +110,46 @@ export default function Home() {
                 <div>{item.content}</div>
                 <div>{item.createdAt}</div>
                 <div>{item.updatedAt}</div>
+                {isEdit && item.id === todoId ? (
+                  <div>
+                    <textarea
+                      defaultValue={item.title}
+                      onChange={(e) => setNewTitle(e.target.value)}
+                      required
+                    />
+                    <textarea
+                      defaultValue={item.content}
+                      onChange={(e) => setNewContent(e.target.value)}
+                      required
+                    />
+                    <button onClick={editModeHandler}>수정 취소</button>
+                    <button onClick={() => updateTodo(item.id)}>완료</button>
+                  </div>
+                ) : (
+                  <div>
+                    <div>{item.title}</div>
+                    <Link to={`/${item.id}`}>
+                      <button type="button">상세</button>
+                    </Link>
+
+                    <button
+                      type="button"
+                      onClick={() => updateHandler(item.id)}
+                    >
+                      수정
+                    </button>
+                <Routes>
+                  <Route
+                    path={`/${item.id}`}
+                    element={
+                      <>
+                        <div>{item.content}</div>
+                        <div>{item.createdAt}</div>
+                        <div>{item.updatedAt}</div>
+                      </>
+                    }
+                  />
+                </Routes>
               </div>
             ))}
         </div>
