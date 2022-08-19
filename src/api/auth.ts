ec7fc83b0fb70f14d46d506c34080ff889fd81f0
seripark13 <qkrtpfl9228@gmail.com>
2022-08-19 16:46:17 +0900

    ♻️ refactor: auth api 가독성 높이기

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,19 +2,18 @@ import { authFormInput } from "../types/auth";
 import { authApi } from "./axiosInstance";
 
 const signUp = ({ email, password }: authFormInput) => {
-  const response = authApi.post("/users/create", {
-    email,
-    password,
-  });
-  return response;
+  return getUserToken("/user/create", { email, password });
 };
 
 const login = ({ email, password }: authFormInput) => {
-  const response = authApi.post("/users/login", {
-    email,
-    password,
-  });
-  return response;
+  return getUserToken("/users/login", { email, password });
 };
 
 export { signUp, login };
+
+const getUserToken = (url: string, { email, password }: authFormInput) => {
+  return authApi.post(url, {
+    email,
+    password,
+  });
+};
